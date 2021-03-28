93375990c4e5ac9b3c64efc381c3117b74ef7e5b
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-03-28 20:11:07 +0300

    feat: add vigenere-cipher and extended-repeat solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  if (options.repeatTimes === undefined) options.repeatTimes = 1;
+  if (options.separator === undefined) options.separator = "+";
+  if (options.addition === undefined) options.addition = "";
+  if (options.additionRepeatTimes === undefined)
+    options.additionRepeatTimes = 1;
+
+  if (options.additionSeparator === undefined)
+    options.additionSeparator = "|";
+
+  const repeatTimes = options.repeatTimes;
+  const separator = options.separator;
+  const addition = options.addition;
+  const additionRepeatTimes = options.additionRepeatTimes;
+  const additionSeparator = options.additionSeparator;
+  
+  let string = String(str);
+
+  string += addition;
+
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    string += additionSeparator + addition;
+  }
+  const repetedString = string;
+  for (let i = 1; i < repeatTimes; i++) {
+    string += separator + repetedString;
+  }
+  return string;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine) {
+    this.machine = machine;
+  }
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    let keycode = key.toUpperCase();
+
+    const messageLength = message.length;
+    keycode = keycode.repeat(Math.ceil(messageLength / key.length)).split('');
+
+    let codeStr = "";
+
+    for(let i = 0; i < messageLength; i += 1) {
+      if(65 <= message.charCodeAt(i) &&  message.charCodeAt(i) < 91) {
+        codeStr += String.fromCharCode(((message.charCodeAt(i) + keycode[0].charCodeAt(0)) % 26) + 65);
+        keycode.shift();
+      } else {
+        codeStr += message.charAt(i);
+      }
+    }
+
+    if(this.machine === false) {
+      return codeStr.split('').reverse().join('');
+    }
+    return codeStr;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    message = message.toUpperCase();
+    let keycode = key.toUpperCase();
+
+    const messageLength = message.length;
+    keycode = keycode.repeat(Math.ceil(messageLength / key.length)).split('');
+
+    let codeStr = "";
+
+    for (let i = 0; i < messageLength; i += 1) {
+      if (65 <= message.charCodeAt(i) &&  message.charCodeAt(i) < 91) {
+        codeStr += String.fromCharCode(((message.charCodeAt(i) + 26 - keycode[0].charCodeAt(0)) % 26) + 65);
+        keycode.shift();
+      } else {
+        codeStr += message.charAt(i);
+      }
+    }
+
+    if (this.machine === false) {
+      return codeStr.split('').reverse().join('');
+    }
+    return codeStr;
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
